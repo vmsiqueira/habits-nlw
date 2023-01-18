7fdce611d94e1d21983ea444b46dcd26070554f8
Vitor <vitormsiqueira@hotmail.com>
2023-01-18 00:26:30 -0300

    fix: mobile dir

---
+++ mobile/src/components/Loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ActivityIndicator, View } from 'react-native';
+
+export function Loading() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <ActivityIndicator color="#7C3AED" />
+    </View>
+  );
+}

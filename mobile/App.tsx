6c60d7da864340dcfdefd74a5729589fc786ceed
Vitor <vitormsiqueira@hotmail.com>
2023-01-22 00:45:46 -0300

    Aula 2 completa

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import './src/lib/dayjs';
+
+import { StatusBar } from 'react-native';
 import { 
   useFonts,
   Inter_400Regular,
@@ -9,6 +10,7 @@ import {
 } from '@expo-google-fonts/inter';
 
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -18,25 +20,16 @@ export default function App() {
     Inter_800ExtraBold
   });
 
-  if(!fontsLoaded) {
+  if (!fontsLoaded) {
     return (
       <Loading />
-    )
+    );
   }
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Home />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,13 @@
-import { Habit } from './components/Habit'
+import { Header } from './components/Header'
+import { SummaryTable } from './components/SummaryTable'
 
 export function App() {
   return (
-    <div className='bg-gray-900 h-screen w-screen'>
-      <Habit completed={3} />
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        <Header />
+        <SummaryTable />
+      </div>
     </div>
   )
 }
